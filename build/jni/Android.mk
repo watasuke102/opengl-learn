7565b4ff0ce9234060d990e1761d8f9688dd628e
watasuke102 <watasuke102@gmail.com>
2024-09-17 13:38:54 +0900

    update: revert shader.cpp

--- build/jni/Android.mk
+++ build/jni/Android.mk
@@ -1,9 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+SRC_DIR          := ../../src
 LOCAL_MODULE     := glesdraw_android
-LOCAL_SRC_FILES  := ../../src/lib.cpp
+LOCAL_SRC_FILES  := $(SRC_DIR)/lib.cpp $(SRC_DIR)/shader.cpp
 LOCAL_LDLIBS     := -lGLESv3
-LOCAL_CPPFLAGS   += -std=c++17 -fno-exceptions -fno-rtti -lGLESv3
+LOCAL_CPPFLAGS   += -I../../inc -std=c++17 -fno-exceptions -fno-rtti -lGLESv3
 
-include $(BUILD_DYNAMIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
